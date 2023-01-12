7d6589a60a4256e02fdfdbdb937e90e672e8748f
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-12 03:36:07 +0100

    mongodb up and running

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 const express = require('express');
-const multer = require('multer');
 const app = express();
 const port = 3000;
 const db = require('./db');
 const routes = require('./routes/FileRoutes')
 
 
-
-// configure Multer to handle file uploads
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-const upload = multer({ storage });
 
 app.use('/api', routes);
 
--- routes/FileRoutes.js
+++ routes/FileRoutes.js
@@ -1,6 +1,9 @@
 const FileController = require('../controllers/FileController');
 const express = require('express');
+const File = require('../models/FileModel')
 const router = express.Router(); // create and store the router instance
+const upload = require('../upload')
+
 
 router.post('/upload', upload.single('file'), FileController.createFile);
 router.get('/file/:name', FileController.getFile);
